249f3d009c56c5315a62ae50f4fad46494cf64d9
Slash <slash@slashware.net>
2024-02-21 23:59:38 -0500

    v0.8.1

--- src/crl/game/Game.java
+++ src/crl/game/Game.java
@@ -662,7 +662,7 @@ public class Game implements CommandListener, PlayerEventListener, java.io.Seria
 	}
 	
 	public static String getVersion(){
-		return "0.8";
+		return "0.8.1";
 	}
 	
 	public void setInterfaces(UserInterface pui, UISelector ps){
